7bc6c91926ada14b4204c69ebedacb793618c855
Hillary Nyakundi <hillarynyakundi66@gmail.com>
2025-06-17 06:30:46 +0300

    fix:me

--- components/ui/TalkButton.tsx
+++ components/ui/TalkButton.tsx
@@ -4,7 +4,7 @@ import Strings from '@/constants/strings';
 const TalkButton = () => {
   return (
     <Link
-      className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-3 rounded-lg font-medium transition-color min-w-[10rem]"
+      className="bg-blue-600 hover:bg-blue-700 text-white px-8 mb-3 py-3 rounded-lg font-medium transition-color min-w-[10rem]"
       href={Strings.calendyLink}
       target="_blank"
     >
